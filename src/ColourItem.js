440ce741d3a114c6e8a1b006cb57f936cdcdabd5
kylegallard <kylegallard@gmail.com>
2021-10-17 15:01:41 +1100

    Initial Commit

---
+++ src/ColourItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ColourData from './ColourData';
+
+const ColourItem = () => {
+    console.log("ColourData ====== " + ColourData());
+    return <h1>Hello</h1>
+}
+
+export default ColourItem;
